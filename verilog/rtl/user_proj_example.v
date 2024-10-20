a29020ad7dca77ac27ee32b2796324b8dd4da3d0
nobleg30 <gnoble30@gmail.com>
2024-10-20 14:47:49 +0530

    Add seven segment seconds and its testbench

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -43,30 +43,14 @@ module user_proj_example #(
     inout vssd1,	// User area 1 digital ground
 `endif
 
-    // Wishbone Slave ports (WB MI A)
     input wb_clk_i,
     input wb_rst_i,
-    input wbs_stb_i,
-    input wbs_cyc_i,
-    input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
-    output [31:0] wbs_dat_o,
 
-    // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
-
-    // IOs
+   // IOs
     input  [BITS-1:0] io_in,
     output [BITS-1:0] io_out,
     output [BITS-1:0] io_oeb,
 
-    // IRQ
-    output [2:0] irq
 );
 
       wire [6:0] led_out;
